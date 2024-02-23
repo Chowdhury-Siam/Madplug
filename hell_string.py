e2bdac5c886511d43216979ab75d8e7b3adb9d4e
MadMax! <159709370+MadMax393@users.noreply.github.com>
2024-02-23 08:11:08 +0530

    Update hell_string.py

--- hell_string.py
+++ hell_string.py
@@ -1,10 +1,3 @@
-import os
-import base64
-import ipaddress
-import random
-import struct
-from random import randint
-
 try:
     from pyrogram import Client as PClient
 except:
@@ -25,28 +18,14 @@ except:
 
 
 def main():
-    print("T E A M    H E L L B O T   ! !")
-    print("Hello!! Welcome to HellBot Extended Session Generator\n")
+    print("T E A M    E X T E N D E D   ! !")
+    print("Hello!! Welcome to Extended Session Generator\n")
     print("Human Verification Required !!")
     while True:
         verify = int(randint(1, 50))
         okvai = int(input(f"Enter {verify} to continue: "))
         if okvai == verify:
-            print("\nChoose the string session type: \n1. HellBot \n2. Telethon \n3. Pyrogram")
-            while True:
-                library = input("\nYour Choice: ")
-                if library == "1":
                     generate_hellbot_session()
-                    break
-                elif library == "2":
-                    generate_telethon_session()
-                    break
-                elif library == "3":
-                    generate_pyro_session()
-                    break
-                else:
-                    print("Please enter integer values (1/2/3 only).")
-            break
         else:
             print("Verification Failed! Try Again:")
 
@@ -64,31 +43,6 @@ def generate_hellbot_session():
             "me",
             f"#HELLEXTENDED_SESSION \n\n`{hell_session}`",
         )
-
-
-def generate_pyro_session():
-    print("Pyrogram Session for Music Bot!")
-    APP_ID = int(input("\nEnter APP ID here: "))
-    API_HASH = input("\nEnter API HASH here: ")
-    with PClient(name="helluser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as hellbot:
-        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-        hellbot.send_message(
-            "me",
-            f"#HELLBOT #PYROGRAM\n\n`{hellbot.export_session_string()}`",
-        )
-
-
-def generate_telethon_session():
-    print("\nTelethon Session For HellBot!")
-    APP_ID = int(input("\nEnter APP ID here: "))
-    API_HASH = input("\nEnter API HASH here: ")
-    with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
-        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-        hellbot.send_message(
-            "me",
-            f"#HELLBOT #TELETHON \n\n`{hellbot.session.save()}`",
-        )
-
 
 
 def challenge_code(username, choice):
@@ -125,35 +79,16 @@ def hellbot_session(session):
         4: "149.154.167.91",
         5: "91.108.56.130",
     }
-
-    error_msg = "Error in generating session! Report it in Hell Chats"
-
-    # converting pyrogram session
-    if len(session) in pyro_format.keys():
-        if len(session) in [351, 356]:
-            dc_id, _, auth_key, _, _ = struct.unpack(
-                pyro_format[len(session)],
-                base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
-            )
-        else:
-            dc_id, _, _, auth_key, _, _ = struct.unpack(
-                pyro_format[len(session)],
-                base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
-            )
-
-        # https://github.com/HellBoy-OP/Telethon/blob/v1/telethon/sessions/string.py
-        new_session = CURRENT_VERSION + StringSession.encode(
-            struct.pack(
-                _STRUCT_PREFORMAT.format(4),
-                dc_id,
-                ipaddress.ip_address(ipv4_dc[dc_id]).packed,
-                443,
-                auth_key
-            )
+   
+    new_session = CURRENT_VERSION + StringSession.encode(
+        struct.pack(
+            _STRUCT_PREFORMAT.format(4),
+            dc_id,
+            ipaddress.ip_address(ipv4_dc[dc_id]).packed,
+            443,
+            auth_key
         )
-        return f"=={hellbot('hell')}{new_session}{hellbot('bot')}=="
-    else:
-        return error_msg
+    )
+    return f"=={hellbot('hell')}{new_session}{hellbot('bot')}=="
 
-
-main()
+main()  # Fix indentation error
